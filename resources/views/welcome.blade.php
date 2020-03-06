bdf45f5291c2429dedea44a4e1b618912f227569
victorhugoeliziani <victorhugoeliziani@gmail.com>
2020-03-05 21:13:23 -0300

    Inicio da Contrução do Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -12,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #1873a3;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
@@ -45,7 +46,17 @@
             }
 
             .title {
-                font-size: 84px;
+                color: #fff;
+                font-size: 40px;
+                font-weight: bold;
+            }
+
+            .title span {
+                margin-top: -20px;
+            }
+
+            .title img {
+                width:15%;
             }
 
             .links > a {
@@ -64,37 +75,29 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <img src="{{url("images/shift.png")}}" >
+                    <span>Ordem de serviço</span>
                 </div>
             </div>
-        </div>
+            <div class="content">
+                <div class="row">
+
+                </div>
+            </div>
+    <script>
+        let todosMedicos = () => {
+
+            axios.get('/api/medicos')
+                .then(function(response){
+                    console.log(response.data); 
+                });
+        }
+
+        todosMedicos();
+    </script>  
     </body>
 </html>
